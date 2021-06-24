0155fb7d43ccd7be625c18f53d6bcb8048e3cf21
Shaid <shaid.bojorquez@gmail.com>
2021-06-24 11:51:10 -0500

    built out navigation and added welcome controller, set root

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+  get 'welcome/about'
+  get 'welcome/contact'
+  get 'welcome/faq'
+  get 'welcome/pricing'
+  get 'welcome/features'
   resources :invoices
   resources :companies do
     collection {post :import}
   end
+
+  root to: 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
